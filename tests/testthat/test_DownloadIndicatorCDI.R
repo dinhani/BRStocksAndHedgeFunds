e0f6a335f0c74b1be2b36dd618782ffe019bcadf
Renato Dinhani <renatodinhani@gmail.com>
2018-10-15 02:26:47 -0300

    Adding tests to CDI indicator

---
+++ tests/testthat/test_DownloadIndicatorCDI.R
@@ -0,0 +1,11 @@
+context("Download Indicator - CDI")
+
+# data
+cdi.daily <- DownloadIndicatorCDIDaily()
+cdi.monthly <- DownloadIndicatorCDIMonthly()
+
+# tests
+test_that("CDI indicator have all columns expected", {
+  expect_equal(colnames(cdi.daily), c("Indicator", "Date", "Value"))
+  expect_equal(colnames(cdi.monthly), c("Indicator", "Date", "Value"))
+})
