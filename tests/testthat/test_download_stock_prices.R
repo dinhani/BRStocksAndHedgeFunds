7197055c151c9c5125f39feb43e7957ac0bd6f7f
Renato Dinhani <renatodinhani@gmail.com>
2018-10-31 23:39:48 -0300

    Removing tests for now

--- tests/testthat/test_download_stock_prices.R
+++
@@ -1,9 +0,0 @@
-context("Download Stock Prices")
-
-# data
-ticker_data <- download_stock_prices("ITUB3")
-
-# tests
-test_that("Stock prices have all expected columns", {
-  expect_equal(colnames(ticker_data), c("Ticker", "Date", "Open", "High", "Low", "Close", "Adjusted", "Volume"))
-})
